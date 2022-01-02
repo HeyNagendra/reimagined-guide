ba088da288633699e94abe2ab2467549ea46f203
HeyNagendra <nagendrak006@gmail.com>
2022-01-02 22:13:11 +0530

    Added array of objects for laptop models

---
+++ simpleObj.js
@@ -0,0 +1,34 @@
+const laptops = [
+    {
+        brand: 'Apple',
+        model: 'Macbook Pro',
+        year: '2019',
+        color: 'Silver',
+        price: '$2,000',
+        weight: '1.5kg',
+        display: '13.3"',
+        processor: 'Intel Core i7',
+    },
+    {
+        brand: 'Dell',
+        model: 'XPS',
+        year: '2019',
+        color: 'Black',
+        price: '$1,000',
+        weight: '1.5kg',
+        display: '13.3"',
+        processor: 'Intel Core i7',
+    },
+    {
+        brand: 'HP',
+        model: 'Pavilion',
+        year: '2019',
+        color: 'Black',
+        price: '$1,000',
+        weight: '1.5kg',
+        display: '13.3"',
+        processor: 'Intel Core i7',
+    }
+];
+
+console.log(laptops[2].brand);
