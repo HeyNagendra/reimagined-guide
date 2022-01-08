b1fd8e402080a88d7e9273558f9bdcb9cd56d892
HeyNagendra <nagendrak006@gmail.com>
2022-01-08 20:34:11 +0530

    add numbers until and  including end number

---
+++ docs/addUntilN.js
@@ -0,0 +1,9 @@
+const endNumber = 10;
+let sum = 0;
+for (let i = 1; i <= endNumber; i++) {
+    sum += i;
+}
+console.log(sum);
+
+const result = endNumber * (endNumber + 1) / 2;
+console.log(result);
